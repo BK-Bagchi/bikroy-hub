2af91c7b8e5914aa7571736b2a815bd6081bfaeb
BK-Bagchi <bkbagchi.dipto@gmail.com>
2023-03-15 00:20:20 +0600

    Bottom section initial construction done

---
+++ src/Components/Bottom/Bottom.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './Bottom.css'
+
+const Bottom = () => {
+    // const date= Date.getFullYear(); 
+    return (
+        <>
+            <section className="bottom container">
+                <div className="row">
+                    <div className="col-sm-4">
+                        <div><p>Download Our App</p></div>
+                        <div><p>Connect With</p></div>
+                        <div><p>Other Countries</p></div>
+                    </div>
+                    <div className="col-sm-2">
+                        <p>More from Bikroy</p>
+                        <ul>
+                            <li>Sell fast</li>
+                            <li>Membership</li>
+                            <li>Banner Ads</li>
+                            <li>Ad promotions</li>
+                            <li>Staff solutions</li>
+                            <li>Bikes Guide</li>
+                        </ul>
+                    </div>
+                    <div className="col-sm-2">
+                        <p>Help & Support</p>
+                        <ul>
+                            <li>FAQ</li>
+                            <li>Stay safe</li>
+                            <li>Contact us</li>
+                        </ul>
+                    </div>
+                    <div className="col-sm-2">
+                        <p>Follow Bikroy</p>
+                        <ul>
+                            <li>Blog</li>
+                            <li>Facebook</li>
+                            <li>Twitter</li>
+                            <li>Youtube</li>
+                        </ul>
+                    </div>
+                    <div className="col-sm-2">
+                        <p>About Bikroy</p>
+                        <ul>
+                            <li>Contact us</li>
+                            <li>Career</li>
+                            <li>Terms and conditions</li>
+                            <li>Privacy policy</li>
+                            <li>Sitemaps</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            <footer>
+                Copyright &copy; bkbagchi.dipto
+            </footer>
+        </>
+    );
+};
+
+export default Bottom;
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import About from '../About/About';
+import Bottom from '../Bottom/Bottom';
 import Categories from '../Categories/Categories';
 import Top from '../Top/Top'
 
@@ -9,6 +10,7 @@ const Main = () => {
             <Top/>
             <Categories/>
             <About/>
+            <Bottom/>
         </div>
     );
 };
