1a6d3937ad0b858d651556c89c025eb0f8f0f4f1
BK-Bagchi <bkbagchi.dipto@gmail.com>
2023-09-22 11:48:51 +0600

    Collected data from edit profile and converted into api for backend use

--- src/Components/MyProfile/EditProfile.js
+++ src/Components/MyProfile/EditProfile.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../Top/Navbar'
 import Bottom from '../Bottom/Bottom'
 import './MyProfile.css'
@@ -8,6 +8,46 @@ const MyProfile = () => {
     const history= useHistory()
     const myName= localStorage.getItem('displayName')
     const myProfilePicture= localStorage.getItem('photoURL')
+
+    const [formData, setFromData]= useState({
+        name: myName,
+        profilePicture: myProfilePicture,
+        location: '',
+        phoneNumber: 0,
+        aboutYou: ''
+    })
+
+    const handelFormInput= (e) =>{
+        const {name, value} = e.target
+        setFromData({
+            ...formData,
+            [name]: value
+        })
+        
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData)
+    
+        // Now, you can make an API call using the formData
+        // For this example, we'll use a fake API endpoint
+        // fetch('https://example.com/api/submit', {
+        //   method: 'POST',
+        //   body: JSON.stringify(formData),
+        //   headers: {
+        //     'Content-Type': 'application/json',
+        //   },
+        // })
+        //   .then((response) => response.json())
+        //   .then((data) => {
+        //     console.log(data);
+        //     // Handle API response here
+        //   })
+        //   .catch((error) => {
+        //     console.error('Error:', error);
+        //   });
+      };
 
   return (
     <>
@@ -20,18 +60,18 @@ const MyProfile = () => {
                     }}>My Profile</p>
             </div>
             <div className="profile-form">
-                <form className="d-flex flex-column align-items-center">
+                <form className="d-flex flex-column align-items-center" onSubmit={handleSubmit}>
                     <div className="profile-picture">
                         <img src={myProfilePicture} alt="User Profile Pic" />
                     </div>
                     <input type="text" value= {myName}/>
-                    <input type="text" placeholder="Enter Your location" />
-                    <input type="number" placeholder="Enter Your Contact"/>
+                    <input type="text" placeholder="Enter Your location" name='location' onBlur={handelFormInput}/>
+                    <input type="number" placeholder="Enter Your Contact" name='phoneNumber' onBlur={handelFormInput}/>
                     <div class="about-user">
                         <p>Write something about yourself so that others can connect to you.</p>
-                        <textarea name="description" placeholder="About you..."></textarea>
+                        <textarea placeholder="About you..." name="aboutYou" onBlur={handelFormInput}></textarea>
                     </div>
-                    <button className='submit-btn'>Submit</button>
+                    <button type='submit' className='submit-btn'>Submit</button>
                 </form>
             </div>
         </section>
