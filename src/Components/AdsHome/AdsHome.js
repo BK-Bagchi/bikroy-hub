07d6bcbf169caf2bf859bb17513edd11782f6a6e
BK-Bagchi <bkbagchi.dipto@gmail.com>
2023-09-28 21:11:29 +0600

    shows ad as per click goes on home page

--- src/Components/AdsHome/AdsHome.js
+++ src/Components/AdsHome/AdsHome.js
@@ -2,9 +2,11 @@
 import React from 'react';
 import './AdsHome.css'
 import adsHomeDatabase from '../Database/AdsHomeDatabse';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 
 const AdsHome = () => {
+    const history = useHistory()
     return (
         <section className="ads-home container">
             <p>Posted adds</p>
@@ -12,8 +14,13 @@ const AdsHome = () => {
                 {
                     adsHomeDatabase.map(adsHome =>{
                         const {id, adImageUrl, adName, adDescription, adTime}= adsHome
+                        
                         return (
-                            <div className={`card ${id}`}>
+                            <div className='card' key={id} onClick={()=>{
+                                localStorage.setItem('adId', id)
+                                history.push('/showAds')
+                                window.location.reload()
+                            }}>
                                 <img className="card-img-top" src={require(`../../Resources/ads/${adImageUrl}`)} alt="Card image cap" />
                                 <div className="card-body">
                                     <h5 className="card-title">{adName}</h5>
@@ -26,26 +33,6 @@ const AdsHome = () => {
                         )
                     })
                 }
-                {/* <div className="card">
-                    <img className="card-img-top" src={require(`../../Resources/ads/Motorcycle.jpg`)} alt="Card image cap" />
-                    <div className="card-body">
-                        <h5 className="card-title">Motorcycle</h5>
-                        <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                    </div>
-                    <div className="card-footer">
-                        <small className="text-muted">Posted 3 days ago</small>
-                    </div>
-                </div>
-                <div className="card">
-                    <img className="card-img-top" src={require(`../../Resources/ads/Laptop.jpg`)} alt="Card image cap" />
-                    <div className="card-body">
-                        <h5 className="card-title">Laptop</h5>
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                    </div>
-                    <div className="card-footer">
-                        <small className="text-muted">Posted 2 days ago</small>
-                    </div>
-                </div> */}
             </div>
         </section>
     );
--- src/Components/ShowAds/ShowAds.js
+++ src/Components/ShowAds/ShowAds.js
@@ -3,59 +3,70 @@ import React from 'react'
 import Navbar from '../Top/Navbar'
 import Bottom from '../Bottom/Bottom'
 import './ShowAds.css'
+import adsHomeDatabase from '../Database/AdsHomeDatabse'
 
 const ShowAds = () => {
+  const adId= localStorage.getItem('adId')
+  const showingAd= adsHomeDatabase.filter(add=> add.id=== parseInt(adId))
+
   return (
     <>
-        <Navbar/>
-        <section className="show-ad container p-2">
-            <p className='ad-name m-2'>Ad name specified by user</p>
-            <div className="ad-picture w-100 d-flex align-items-center justify-content-center">
-              <img className="picture w-25" src={require(`../../Resources/ads/Laptop.jpg`)} alt="picture" />
-            </div>
-            <div className='description d-flex flex-column align-items-center justify-content-center my-3'>
-              <div className="product-price">
-                <p className="price">Tk. 1000</p>
-              </div>
-              <div className="details d-flex flex-wrap">
-              {/* dynamically data will come from db at this place */}
-                <div>
-                  <h6>Condition</h6>
-                  <p>Condition</p>
+      <Navbar/>
+        {
+          showingAd.map(thisAd=>{
+            const {id, adImageUrl, adName, adDescription, adTime}= thisAd
+
+            return(
+              <section className={`show-ad container p-2 ${adTime}`} key={id}>
+                <p className='ad-name m-2'>{adName}</p>
+                <div className="ad-picture w-100 d-flex align-items-center justify-content-center">
+                  <img className="picture w-25" src={require(`../../Resources/ads/${adImageUrl}`)} alt="picture" />
                 </div>
-                <div>
-                  <h6>Brand</h6>
-                  <p>Brand</p>
+                <div className='description d-flex flex-column align-items-center justify-content-center my-3'>
+                  <div className="product-price">
+                    <p className="price">Tk. 1000</p>
+                  </div>
+                  <div className="details d-flex flex-wrap">
+                  {/* dynamically data will come from db at this place */}
+                    <div>
+                      <h6>Condition</h6>
+                      <p>Condition</p>
+                    </div>
+                    <div>
+                      <h6>Brand</h6>
+                      <p>Brand</p>
+                    </div>
+                    <div>
+                      <h6>Model</h6>
+                      <p>Model</p>
+                    </div>
+                    <div>
+                      <h6>Condition</h6>
+                      <p>Condition</p>
+                    </div>
+                    <div>
+                      <h6>Type</h6>
+                      <p>Type</p>
+                    </div>
+                    <div>
+                      <h6>Model</h6>
+                      <p>Model</p>
+                    </div>
+                    <div>
+                      <h6>Condition</h6>
+                      <p>Condition</p>
+                    </div>
+                  </div>
+                  <div className="description">
+                    <p>{adDescription}</p>
+                  </div>
                 </div>
-                <div>
-                  <h6>Model</h6>
-                  <p>Model</p>
-                </div>
-                <div>
-                  <h6>Condition</h6>
-                  <p>Condition</p>
-                </div>
-                <div>
-                  <h6>Type</h6>
-                  <p>Type</p>
-                </div>
-                <div>
-                  <h6>Model</h6>
-                  <p>Model</p>
-                </div>
-                <div>
-                  <h6>Condition</h6>
-                  <p>Condition</p>
-                </div>
-              </div>
-              <div className="description">
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque tenetur praesentium sint sunt! Molestiae, culpa. Incidunt pariatur veritatis, ab in consectetur, deleniti sequi consequatur dicta omnis dolorem nobis tenetur dolorum expedita tempore enim! Debitis a molestias tempora explicabo, obcaecati corporis nemo sit nisi soluta ducimus perspiciatis tempore iure error eveniet.
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque tenetur praesentium sint sunt! Molestiae, culpa. Incidunt pariatur veritatis, ab in consectetur, deleniti sequi consequatur dicta omnis dolorem nobis tenetur dolorum expedita tempore enim! Debitis a molestias tempora explicabo, obcaecati corporis nemo sit nisi soluta ducimus perspiciatis tempore iure error eveniet.
-                </p>
-              </div>
-            </div>
-        </section>
-        <Bottom/>
+                
+              </section>
+            )
+          })
+        }
+      <Bottom/>
     </>
   )
 }
