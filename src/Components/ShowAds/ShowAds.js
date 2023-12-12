28422d185b4c93ecf603f90f8f7d046844674da2
BK-Bagchi <bkbagchi.dipto@gmail.com>
2023-12-12 11:16:41 +0600

    shows ad as per click goes on home page

--- src/Components/ShowAds/ShowAds.js
+++ src/Components/ShowAds/ShowAds.js
@@ -1,64 +1,68 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../Top/Navbar'
 import Bottom from '../Bottom/Bottom'
 import './ShowAds.css'
 import adsHomeDatabase from '../Database/AdsHomeDatabse'
+import axios from 'axios'
 
 const ShowAds = () => {
   const adId= localStorage.getItem('adId')
-  const showingAd= adsHomeDatabase.filter(add=> add.id=== parseInt(adId))
+  const [adsInfo, setAdsInfo]= useState([])
+
+  useEffect(()=>{
+    axios.get('http://localhost:4000/getAdsInfo')
+      .then(response => {
+        // console.log('Response:', response.data);
+        setAdsInfo(response.data)
+      })
+      .catch(error => {
+        // Handle errors here
+        console.error('Error:', error.message);
+      });
+  },[])
+  const showingAd= adsInfo.filter(add=> add._id === adId)
 
   return (
     <>
       <Navbar/>
-        {
+      {
           showingAd.map(thisAd=>{
-            const {id, adImageUrl, adName, adDescription, adTime}= thisAd
+            {/* const {_id, adImageUrl, adName, adDescription, adTime}= thisAd */}
+            const {_id, itemName, price, category, description}= thisAd
 
             return(
-              <section className={`show-ad container p-2 ${adTime}`} key={id}>
-                <p className='ad-name m-2'>{adName}</p>
+              <section className={`show-ad container p-2 {adTime}`} key={_id}>
+                <p className='ad-name m-2'>{itemName}</p>
+                <button className='buy-now'>Buy Now</button>
                 <div className="ad-picture w-100 d-flex align-items-center justify-content-center">
-                  <img className="picture w-25" src={require(`../../Resources/ads/${adImageUrl}`)} alt="picture" />
+                  <img className="picture w-25" src={require(`../../Resources/ads/Laptop.jpg`)} alt="picture" />
                 </div>
                 <div className='description d-flex flex-column align-items-center justify-content-center my-3'>
                   <div className="product-price">
-                    <p className="price">Tk. 1000</p>
+                    <p className="price">Tk. {price}</p>
                   </div>
                   <div className="details d-flex flex-wrap">
                   {/* dynamically data will come from db at this place */}
                     <div>
-                      <h6>Condition</h6>
-                      <p>Condition</p>
-                    </div>
-                    <div>
-                      <h6>Brand</h6>
+                      <h6>Brand Name</h6>
                       <p>Brand</p>
                     </div>
                     <div>
                       <h6>Model</h6>
-                      <p>Model</p>
+                      <p>{itemName}</p>
                     </div>
                     <div>
                       <h6>Condition</h6>
                       <p>Condition</p>
                     </div>
                     <div>
-                      <h6>Type</h6>
-                      <p>Type</p>
-                    </div>
-                    <div>
-                      <h6>Model</h6>
-                      <p>Model</p>
-                    </div>
-                    <div>
-                      <h6>Condition</h6>
-                      <p>Condition</p>
+                      <h6>Category</h6>
+                      <p>{category}</p>
                     </div>
                   </div>
                   <div className="description">
-                    <p>{adDescription}</p>
+                    <p>{description}</p>
                   </div>
                 </div>
                 
