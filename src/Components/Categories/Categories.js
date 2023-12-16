c3ee45a1f9902cefef7009ebf8a3ccae6ec0ba24
BK-Bagchi <bkbagchi.dipto@gmail.com>
2023-12-16 20:24:02 +0600

    styling at category at home section

--- src/Components/Categories/Categories.js
+++ src/Components/Categories/Categories.js
@@ -32,32 +32,36 @@ const Categories = () => {
                 <div className="col-sm-3">
                     <div className="card">
                         <div className="card-body">
+                            <img className='card-icon' src={require(`../../Resources/images/mobile-icon.png`)} alt="mobile-icon" />
                             <h5 className="card-title">Mobile</h5>
-                            <p className="card-text">Total Amount: {countAdByCategory.Mobile}</p>
+                            <p className="card-text">Total Ads: <b>{countAdByCategory.Mobile}</b></p>
                         </div>
                     </div>
                 </div>
                 <div className="col-sm-3">
                     <div className="card">
                         <div className="card-body">
+                            <img className='card-icon' src={require(`../../Resources/images/electronics-icon.png`)} alt="electronics-icon" />
                             <h5 className="card-title">Electronics</h5>
-                            <p className="card-text">Total Amount: {countAdByCategory.Electronic}</p>
+                            <p className="card-text">Total Ads: <b>{countAdByCategory.Electronic}</b></p>
                         </div>
                     </div>
                 </div>
                 <div className="col-sm-3">
                     <div className="card">
                         <div className="card-body">
+                            <img className='card-icon' src={require(`../../Resources/images/vehicle-icon.jpeg`)} alt="vehicle-icon" />
                             <h5 className="card-title">Vehicle</h5>
-                            <p className="card-text">Total Amount: {countAdByCategory.Vehicle}</p>
+                            <p className="card-text">Total Ads: <b>{countAdByCategory.Vehicle}</b></p>
                         </div>
                     </div>
                 </div>
                 <div className="col-sm-3">
                     <div className="card">
                         <div className="card-body">
+                            <img className='card-icon' src={require(`../../Resources/images/laptop-icon.png`)} alt="laptop-icon" />
                             <h5 className="card-title">Laptop</h5>
-                            <p className="card-text">Total Amount: {countAdByCategory.Laptop}</p>
+                            <p className="card-text">Total Ads: <b>{countAdByCategory.Laptop}</b></p>
                         </div>
                     </div>
                 </div>
