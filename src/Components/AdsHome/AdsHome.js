20364f98655d565d9a62ccedc4de9676633540b3
unknown <bkbagchi.dipto@gmail.com>
2025-09-02 16:29:01 +0600

    User himself can't see his posted adds as add

--- src/Components/AdsHome/AdsHome.js
+++ src/Components/AdsHome/AdsHome.js
@@ -3,14 +3,17 @@ import React, { useEffect, useState } from "react";
 import "./AdsHome.css";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import axios from "axios";
+import useAuth from "../../Hooks/JWTDecode";
 
 const AdsHome = () => {
   const history = useHistory();
+  const { user } = useAuth();
   const [adsInfo, setAdsInfo] = useState([]);
   const [showLoader, setShowLoader] = useState(true);
   const showAdsOnLoad = 3;
   const [adsToShow, setAdsToShow] = useState(showAdsOnLoad); //at start show how many ads
-  const showAdsHome = adsInfo.slice(-adsToShow).reverse();
+  const filteredAdsToShow = adsInfo.filter((ad) => ad.email !== user.email);
+  const showAdsHome = filteredAdsToShow.slice(-adsToShow).reverse();
   const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
   useEffect(() => {
