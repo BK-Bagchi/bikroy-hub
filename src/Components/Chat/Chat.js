a64baf1fc15831e5b8a5f6bb48faa2ed70891851
unknown <bkbagchi.dipto@gmail.com>
2025-09-21 15:35:00 +0600

    Seller and buyer can see their pic during chatting

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -5,12 +5,13 @@ import "./Chat.css";
 // Connect to backend Socket.IO
 const socket = io(process.env.REACT_APP_API_BASE_URL);
 const Chat = ({ buyerEmail, sellerEmail, user }) => {
-  //   console.log(process.env.REACT_APP_API_BASE_URL);
-  //   console.log(buyerEmail, sellerEmail, user);
+  const [showLoader, setShowLoader] = useState(true);
   const [currentUserEmail, setCurrentUserEmail] = useState("");
   const [partnerEmail, setPartnerEmail] = useState("");
+  const [userInfo, setUserInfo] = useState([]);
   const [messages, setMessages] = useState([]);
   const [text, setText] = useState("");
+  console.log(messages);
 
   useEffect(() => {
     if (user === "buyer") {
@@ -32,17 +33,20 @@ const Chat = ({ buyerEmail, sellerEmail, user }) => {
         partnerEmail: currentUserEmail,
       });
     }
-    socket.emit("register", { email: currentUserEmail, partnerEmail });
     socket.on("loadMessages", (msgs) => {
       setMessages(msgs);
+      setShowLoader(false);
     });
     socket.on("privateMessage", (msg) => {
       setMessages((prev) => [...prev, msg]);
     });
-
+    socket.on("loadProfile", (data) => {
+      setUserInfo(data);
+    });
     return () => {
       socket.off("loadMessages");
       socket.off("privateMessage");
+      socket.off("loadProfile");
     };
   }, [currentUserEmail, partnerEmail, user]);
 
@@ -87,59 +91,65 @@ const Chat = ({ buyerEmail, sellerEmail, user }) => {
     }
   }, [currentUserEmail, partnerEmail, user]);
 
-  return (
+  return showLoader ? (
+    <p className="text-center">Loading...</p>
+  ) : (
     <div className="chat-box-2">
       {/* Header */}
       <div className="chat-header d-flex align-items-center px-3 py-2">
         {messages.length > 0 ? (
-          <>
-            <img
-              src="https://i.pravatar.cc/40?img=5"
-              alt="Seller"
-              className="rounded-circle me-2"
-            />
-            <h6 className="mb-0">Seller Name</h6>{" "}
-          </>
+          (() => {
+            const thisUser =
+              currentUserEmail === userInfo.buyerInfo?.email
+                ? userInfo.buyerInfo
+                : userInfo.sellerInfo;
+
+            return (
+              <>
+                {/* prettier-ignore */}
+                <img src={thisUser?.photoURL} alt="User"  className="user-avatar-strong rounded-circle me-2" />
+                <h6 className="mb-0">
+                  {thisUser?.displayName || "Unknown User"}
+                </h6>
+              </>
+            );
+          })()
         ) : (
-          <p>No previous message to show. Start conversion</p>
+          <p>No previous message to show. Start conversation</p>
         )}
       </div>
 
       {/* Messages */}
       <div className="chat-messages p-3">
-        {/* <div className="message received mb-3">
-          <p>Hello, how can I help you?</p>
-          <span className="time">10:30 AM</span>
-        </div>
-        <div className="message sent mb-3">
-          <p>Is this item still available?</p>
-          <span className="time">10:31 AM</span>
-        </div> */}
         {messages &&
-          messages.map((msg, index) =>
-            currentUserEmail === msg.sender ? (
-              <div className="message sent mb-3" key={index}>
-                <p>{msg.text}</p>
-                <span>{new Date(msg.createdAt).toLocaleString()}</span>
+          messages.map((msg, index) => {
+            const isCurrentUser = currentUserEmail === msg.sender;
+
+            const profilePic = isCurrentUser
+              ? userInfo.buyerInfo?.photoURL
+              : userInfo.sellerInfo?.photoURL;
+
+            return (
+              // prettier-ignore
+              <div className={`message ${ isCurrentUser ? "sent" : "received" } mb-3 d-flex align-items-start`} key={index} >
+                {/* prettier-ignore */}
+                <img src={profilePic} alt="User"  className="user-avatar rounded-circle me-2" />
+                <div>
+                  <p className="mb-1 msg-text">{msg.text}</p>
+                  {/* prettier-ignore */}
+                  <span className="text-muted date-text" style={{ fontSize: "0.75rem" }} >
+                    {new Date(msg.createdAt).toLocaleString()}
+                  </span>
+                </div>
               </div>
-            ) : (
-              <div className="message received mb-3" key={index}>
-                <p>{msg.text}</p>
-                <span>{new Date(msg.createdAt).toLocaleString()}</span>
-              </div>
-            )
-          )}
+            );
+          })}
       </div>
 
       {/* Input */}
       <div className="chat-input d-flex p-2">
-        <input
-          type="text"
-          className="form-control me-2"
-          placeholder="Type a message..."
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
+        {/* prettier-ignore */}
+        <input type="text" className="form-control me-2" placeholder="Type a message..." value={text} onChange={(e) => setText(e.target.value)} />
         <button className="btn btn-primary" onClick={handleSendMsg}>
           <i className="bi bi-send"></i>
         </button>
